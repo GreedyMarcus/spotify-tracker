20a731a1d4cea7b1fb4134788d1d1eb19d5aced1
Domahidi Márk <mdomahidi@gmail.com>
2022-02-03 16:36:30 +0100

    Implement endpoint to retrieve profile information

---
+++ src/pages/api/profile.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import SpotifyWebApi from "spotify-web-api-node";
+
+/**
+ * Retrieve detailed profile information about the current user.
+ */
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    res.status(400).send({ message: "Only GET request allowed" });
+    return;
+  }
+
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI
+  });
+
+  spotifyApi.setAccessToken(req.headers.authorization ?? "");
+
+  const myProfile = spotifyApi.getMe();
+  const myTopArtists = spotifyApi.getMyTopArtists({ limit: 3 });
+  const myTopTracks = spotifyApi.getMyTopTracks({ limit: 3 });
+
+  try {
+    const [profile, artists, tracks] = await Promise.all([myProfile, myTopArtists, myTopTracks]);
+
+    res.json({
+      profile: {
+        id: profile.body.id,
+        name: profile.body.display_name,
+        followers: profile.body.followers?.total ?? 0,
+        image: profile.body.images?.length ? profile.body.images[0].url : null
+      },
+      artists: artists.body.items.map(({ id, name, images }) => ({
+        id,
+        name,
+        image: images.length ? images[0].url : null
+      })),
+      tracks: tracks.body.items.map(({ id, name, duration_ms, album }) => ({
+        id,
+        name,
+        duration: duration_ms / 1000,
+        album: {
+          id: album.id,
+          name: album.name,
+          image: album.images.length ? album.images[0].url : null
+        }
+      }))
+    });
+  } catch (err) {
+    res.status(500);
+  }
+}
